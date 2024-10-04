253da704bc067cb3cfbb48b2f6228d6523e1fd02
keeeg4n <sathvikworks@gmail.com>
2024-10-04 14:44:44 +0530

    Project Setup

---
+++ src/main/java/dev/keeg4n/application/Application.java
@@ -0,0 +1,13 @@
+package dev.keeg4n.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}

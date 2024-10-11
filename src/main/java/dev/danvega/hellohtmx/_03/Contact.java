0c8b1d57167c060330d299ae6bd5864733f213ab
keeeg4n <sathvikworks@gmail.com>
2024-10-11 14:00:02 +0530

    Setup

--- src/main/java/dev/danvega/hellohtmx/_03/Contact.java
+++
@@ -1,5 +0,0 @@
-package dev.danvega.hellohtmx._03;
-
-public record Contact(String name, String email, String id) {
-
-}
--- src/main/java/dev/danvega/hellohtmx/_04/User.java
+++
@@ -1,4 +0,0 @@
-package dev.danvega.hellohtmx._04;
-
-public record User(String name, String title, String email, String role) {
-}
